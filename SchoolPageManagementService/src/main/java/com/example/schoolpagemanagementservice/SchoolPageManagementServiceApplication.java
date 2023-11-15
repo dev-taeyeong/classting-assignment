d9d696c43a564433f2978ed0b6647552c04a6c36
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-15 22:14:49 +0900

    Initialize spring boot project SchoolPageManagementService (#1)

---
+++ SchoolPageManagementService/src/main/java/com/example/schoolpagemanagementservice/SchoolPageManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolpagemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPageManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolPageManagementServiceApplication.class, args);
+    }
+
+}
