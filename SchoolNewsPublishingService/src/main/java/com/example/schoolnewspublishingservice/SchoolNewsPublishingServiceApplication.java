cb71ba3b86b997867678d1235d07c93ff058db1e
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-16 02:03:39 +0900

    Initialize Spring Boot project - SchoolNewsPublishservice (#2)

---
+++ SchoolNewsPublishingService/src/main/java/com/example/schoolnewspublishingservice/SchoolNewsPublishingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolnewspublishingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolNewsPublishingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolNewsPublishingServiceApplication.class, args);
+    }
+
+}
---
+++ SchoolNewsPublishingService/src/test/java/com/example/schoolnewspublishingservice/SchoolNewsPublishingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolnewspublishingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolNewsPublishingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
