ce226bc816c348983bc60b30e72884dd3b0a2d97
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-16 00:12:08 +0900

    Feat: Add fetchAdministratorById() method (#1)

--- SchoolPageManagementService/src/main/java/com/example/schoolpagemanagementservice/application/out/CreateSchoolPageOutputPort.java
+++ SchoolPageManagementService/src/main/java/com/example/schoolpagemanagementservice/application/out/CreateSchoolPageOutputPort.java
@@ -1,8 +1,13 @@
 package com.example.schoolpagemanagementservice.application.out;
 
+import com.example.schoolpagemanagementservice.domain.model.Administrator;
 import com.example.schoolpagemanagementservice.domain.model.SchoolPage;
+
+import java.util.Optional;
 
 public interface CreateSchoolPageOutputPort {
 
     SchoolPage save(SchoolPage schoolPage);
+
+    Optional<Administrator> fetchAdministratorById(long administratorId);
 }
--- SchoolPageManagementService/src/main/java/com/example/schoolpagemanagementservice/framework/out/CreateSchoolPageAdapter.java
+++ SchoolPageManagementService/src/main/java/com/example/schoolpagemanagementservice/framework/out/CreateSchoolPageAdapter.java
@@ -1,19 +1,29 @@
 package com.example.schoolpagemanagementservice.framework.out;
 
 import com.example.schoolpagemanagementservice.application.out.CreateSchoolPageOutputPort;
+import com.example.schoolpagemanagementservice.domain.model.Administrator;
 import com.example.schoolpagemanagementservice.domain.model.SchoolPage;
+import com.example.schoolpagemanagementservice.framework.out.jpa.AdministratorJpaRepository;
 import com.example.schoolpagemanagementservice.framework.out.jpa.SchoolPageJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
 public class CreateSchoolPageAdapter implements CreateSchoolPageOutputPort {
 
     private final SchoolPageJpaRepository schoolPageJpaRepository;
+    private final AdministratorJpaRepository administratorJpaRepository;
 
     @Override
     public SchoolPage save(SchoolPage schoolPage) {
         return schoolPageJpaRepository.save(schoolPage);
+    }
+
+    @Override
+    public Optional<Administrator> fetchAdministratorById(long administratorId) {
+        return administratorJpaRepository.findById(administratorId);
     }
 }
