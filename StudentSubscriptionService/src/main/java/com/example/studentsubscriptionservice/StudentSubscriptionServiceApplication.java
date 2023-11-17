37f1654376fc43a2e86c30337e6380228685daae
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-17 17:04:15 +0900

    Initialize Spring Boot project - StudentSubscriptionService (#3)

---
+++ StudentSubscriptionService/src/main/java/com/example/studentsubscriptionservice/StudentSubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentsubscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSubscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSubscriptionServiceApplication.class, args);
+    }
+
+}
---
+++ StudentSubscriptionService/src/test/java/com/example/studentsubscriptionservice/StudentSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentsubscriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSubscriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
