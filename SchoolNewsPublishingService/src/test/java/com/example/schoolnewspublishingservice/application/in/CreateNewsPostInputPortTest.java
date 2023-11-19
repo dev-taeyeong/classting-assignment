f5d5e310c6bf07f5c761978a02d1adbd04a3032b
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-20 04:20:31 +0900

    Test: Refactoring CreateNewsPostInputPortTest (#2)

--- SchoolNewsPublishingService/src/test/java/com/example/schoolnewspublishingservice/application/in/CreateNewsPostInputPortTest.java
+++ SchoolNewsPublishingService/src/test/java/com/example/schoolnewspublishingservice/application/in/CreateNewsPostInputPortTest.java
@@ -3,6 +3,7 @@ package com.example.schoolnewspublishingservice.application.in;
 import com.example.schoolnewspublishingservice.application.in.dto.CreateNewsPostCommand;
 import com.example.schoolnewspublishingservice.application.in.dto.NewsPostDto;
 import com.example.schoolnewspublishingservice.application.out.CreateNewsPostOutputPort;
+import com.example.schoolnewspublishingservice.application.out.SchoolPagePermissionOutputPort;
 import com.example.schoolnewspublishingservice.domain.model.NewsPost;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,9 @@ class CreateNewsPostInputPortTest {
     @Mock
     private CreateNewsPostOutputPort createNewsPostOutputPort;
 
+    @Mock
+    private SchoolPagePermissionOutputPort schoolPagePermissionOutputPort;
+
     @DisplayName("뉴스 포스트 생성 시 권한 확인 후 NewsPostDto를 반환한다")
     @Test
     void givenCreateNewsPostCommandWithValidPermission_whenCreatingNewsPost_thenReturnNewsPostDto() {
@@ -32,6 +36,8 @@ class CreateNewsPostInputPortTest {
         CreateNewsPostCommand command = new CreateNewsPostCommand(1L, 2L, "test title", "test content");
         NewsPost newsPost = NewsPost.createNewsPost(command.schoolPageId(), command.title(), command.content());
         when(createNewsPostOutputPort.save(any(NewsPost.class))).thenReturn(newsPost);
+        when(schoolPagePermissionOutputPort.checkPermission(command.administratorId(), command.schoolPageId()))
+                .thenReturn(true);
 
         // when
         NewsPostDto result = sut.createNewsPost(command);
@@ -42,5 +48,6 @@ class CreateNewsPostInputPortTest {
         assertThat(result.content()).isEqualTo(command.content());
 
         then(createNewsPostOutputPort).should().save(any(NewsPost.class));
+        then(schoolPagePermissionOutputPort).should().checkPermission(command.administratorId(), command.schoolPageId());
     }
 }
