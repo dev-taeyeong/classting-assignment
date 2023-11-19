1bdaa6895a662668ad2ce25959f7470fd333dfa6
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-19 22:33:05 +0900

    Test: Add SchoolPageApiControllerTest (#3)

--- StudentSubscriptionService/src/test/java/com/example/studentsubscriptionservice/framework/in/web/StudentApiControllerTest.java
+++ StudentSubscriptionService/src/test/java/com/example/studentsubscriptionservice/framework/in/web/StudentApiControllerTest.java
@@ -8,20 +8,14 @@ import com.example.studentsubscriptionservice.application.usecase.GetAllSubscrip
 import com.example.studentsubscriptionservice.framework.in.web.request.CreateStudentRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
