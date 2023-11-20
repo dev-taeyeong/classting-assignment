7c77a45e056aa03995ef45b95b0e7fc4e69f9082
dev-taeyeong <dev.taeyeong@gmail.com>
2023-11-20 10:02:36 +0900

    Test: Add newsfeeds.service.spec.ts (#4)

--- newsfeed-service/src/newsfeeds/newsfeeds.service.spec.ts
+++ newsfeed-service/src/newsfeeds/newsfeeds.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsfeedsService } from './newsfeeds.service';
+import { Repository } from 'typeorm';
+import { Newsfeed } from './entities/newsfeed.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('NewsfeedsService', () => {
   let service: NewsfeedsService;
+  let repository: Repository<Newsfeed>;
+
+  const mockNewsfeedRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewsfeedsService],
+      providers: [
+        NewsfeedsService,
+        {
+          provide: getRepositoryToken(Newsfeed),
+          useValue: mockNewsfeedRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<NewsfeedsService>(NewsfeedsService);
+    repository = module.get<Repository<Newsfeed>>(getRepositoryToken(Newsfeed));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create and return a newsfeed', async () => {
+      const createNewsfeedDto = { studentId: 1, newsPostId: '123' };
+      const expectedNewsfeed = { id: 1, ...createNewsfeedDto };
+
+      jest.spyOn(repository, 'create').mockReturnValue(expectedNewsfeed);
+      jest.spyOn(repository, 'save').mockResolvedValue(expectedNewsfeed);
+
+      const result = await service.create(createNewsfeedDto);
+      expect(result).toEqual(expectedNewsfeed);
+    });
   });
 });
